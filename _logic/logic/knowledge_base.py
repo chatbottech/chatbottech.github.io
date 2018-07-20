07cca61684d09c2746e13c1057c42cc73729a3d3
Daoud Clarke <daoud.clarke@gmail.com>
2018-07-20 07:54:53 +0100

    Use a knowledgebase

---
+++ _logic/logic/knowledge_base.py
@@ -0,0 +1,24 @@
+
+
+class KnowledgeBase:
+    def say(self, saying, meaning):
+        pass
+
+    def hear(self, saying, meaning):
+        pass
+
+    def rule(self, statement, condition=None):
+        pass
+
+
+def what(variable):
+    pass
+
+
+def is_(condtion):
+    pass
+
+
+def months(number):
+    pass
+
--- _logic/tests/logic_test.py
+++ _logic/tests/logic_test.py
@@ -1,19 +1,20 @@
-from logic.operators import what, say, is_, hear, months, rule
+from logic.knowledge_base import KnowledgeBase, what, is_, months
 from logic.variables import Duration, Option
 
 
 def test_simple():
-    visa_duration = Duration('duration')
+    kb = KnowledgeBase()
+
+    visa_duration = Duration('visa_duration')
     visa_purpose = Option('visa_purpose')
     visa_type = Option('visa_type')
 
-    say("How long do you want to come to the UK for?", what(visa_duration))
-    say("What is the purpose of your visit?", what(visa_purpose))
-    say("Are you coming for business?", is_(visa_purpose == 'business'))
-    say("You can get a short term study visa", visa_type == 'short_term_study')
+    kb.say("How long do you want to come to the UK for?", what(visa_duration))
+    kb.say("What is the purpose of your visit?", what(visa_purpose))
+    kb.say("Are you coming for business?", is_(visa_purpose == 'business'))
+    kb.say("You can get a short term study visa", visa_type == 'short_term_study')
 
-    hear("I'm coming to study.", visa_purpose == 'study')
-    hear("I want to come for five months.", visa_duration == months(5))
+    kb.hear("I'm coming to study.", visa_purpose == 'study')
+    kb.hear("I want to come for five months.", visa_duration == months(5))
 
-    rule(visa_type == 'short_study' in (visa_duration <= months(6)
-                                        and visa_purpose == 'study'))
+    kb.rule(visa_type == 'short_term_study', visa_duration <= months(6) and visa_purpose == 'study')
